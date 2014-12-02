d9f0dff9ee10d91fa02f1587455c086d26592acc
Rebecca Appleyard <rklappleyard@gmail.com>
2014-12-02 13:48:42 +0000

    update ex7.py

---
+++ ex7.py
@@ -0,0 +1,27 @@
+#print string
+print "Mary had a little lamb."
+#print string including second string snow
+print "Its fleece was white as %s." % 'snow'
+#print string
+print "And everywhere that Mary went."
+
+print "." * 10 #what'd that do? - it prints . 10 times
+
+#defines 12 variables, each with a single letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end.  try removing it to see what happens - it keeps it on one line
+#prints variables to make two strings on the same line 
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
