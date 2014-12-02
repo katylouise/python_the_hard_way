2e286120d23787eb5665935d870b694eecc1b25f
Rebecca Appleyard <rklappleyard@gmail.com>
2014-12-02 11:05:34 +0000

    update ex3.py

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#prints a string
+print "I will now count my chickens:"
+#prints a string and answer to division/addition
+print "Hens", 25 + 30 / 6
+#prints string and answer to muliply, modulus, subtract 100 - 3 as 75 % 4 is 3.
+print "Roosters", 100 - 25 * 3 % 4
+#print string
+print "Now I will count the eggs:"
+#prints answer to 5 % 2 which is 1, then subtract 1/4 which is 0, so answer 1 still, then add 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2 - 1.0 / 4.0 + 6.0
+#prints string
+print "Is it true that 3 + 2 < 5 - 7?"
+#prints answer (true/false) to inequality
+print 3 + 2 < 5 - 7
+#print string and answer to addition
+print "What is 3 + 2?", 3 + 2
+#print string and answer to subtraction
+print "What is 5 - 7?", 5 - 7
+#print string
+print "Oh, that's why it's False."
+#print string
+print "How about some more."
+#print string and answer (t/f) to inequality more than
+print "Is it greater?", 5 > -2
+#print string and answer (t/f) to inequality more than or equal to
+print "Is it greater or equal?", 5 >= -2
+#print string and answer (t/f) to inequality less than or equal to
+print "Is it less or equal?", 5 <= -2
